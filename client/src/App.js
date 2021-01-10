0e1540ca861bf000a8b60cfed4ed8db3ee6dd507
Sheraz Haider <sheraz5006@gmail.com>
2021-01-10 22:59:11 +0500

    Setup Redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,26 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+// Redux
+import { Provider } from 'react-redux';
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route path='/' exact component={Landing} />
-        <section className='container'>
-          <Switch>
-            <Route path='/login' exact component={Login} />
-            <Route path='/register' exact component={Register} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route path='/' exact component={Landing} />
+          <section className='container'>
+            <Switch>
+              <Route path='/login' exact component={Login} />
+              <Route path='/register' exact component={Register} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
