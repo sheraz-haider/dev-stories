81a08246b74a2007b83c37870d440c4ac209475e
Sheraz Haider <sheraz5006@gmail.com>
2021-01-24 18:34:40 +0500

    Redirect to dashboard if logged in

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,11 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import useStore from '../../hooks/useStore';
 
 function Landing() {
+  const [
+    {
+      auth: { isLoggedIn },
+    },
+  ] = useStore();
+
+  if (isLoggedIn) {
+    return <Redirect to='/dashboard' />;
+  }
+
   return (
     <section className='landing'>
       <div className='dark-overlay'>
         <div className='landing-inner'>
-          <h1 className='x-large'>Developer Connector</h1>
+          <h1 className='x-large'>Developer Stories</h1>
           <p className='lead'>
             Create a developer profile/portfolio, share posts and get help from
             other developers
